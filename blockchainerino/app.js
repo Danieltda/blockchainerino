fb71d3f2716bd07c1be3c7555d93c64fbf019d92
Daniel td <danieltd@gmail.com>
2021-09-25 11:20:46 +0200

    added the auth login part

--- blockchainerino/app.js
+++ blockchainerino/app.js
@@ -18,15 +18,18 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-// default value for title local
-const projectName = "blockchainerino";
-const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
+const projectName = "test";
+const capitalized = (string) =>
+  string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
+
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
