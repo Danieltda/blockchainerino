cdc7164b105c2fda96692de983fec8f3c7b504bd
Daniel td <danieltd@gmail.com>
2021-10-02 15:22:00 +0200

    dynamic coin and price

--- blockchainerino/public/js/script.js
+++ blockchainerino/public/js/script.js
@@ -7,16 +7,29 @@ document.addEventListener(
 );
 
 //update current price based on coin
-var select = document.getElementById("coinname");
-var value = select.options[select.selectedIndex].text;
-console.log(value);
-
 function update() {
   var select = document.getElementById("coinname");
+  console.log(select);
+  // var select1 = document.getElementById("price");
+  // console.log(select1);
   var option = select.options[select.selectedIndex];
+  console.log(option);
+  // var option1 = select1.options[select1.selectedIndex].value;
+  // console.log(option1);
+  // var price = document.getElementById("price").value;
+  // console.log(price);
 
-  document.getElementById("coinname").value = option.text;
-  document.getElementById("current-price").value = option.text;
+  let coinName = (document.getElementById("coinname").value = option.value);
+  let currentPrice = (document.getElementById("current-price").value =
+    select.value);
 }
 
 update();
+
+//function to update the total value
+// Current price times the crypto amount
+function totalValue(coinPrice, totalAmount) {
+  let totalValue = document.getElementById("total-value");
+
+  totalValue = coinPrice * totalAmount;
+}
